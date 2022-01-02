c0782d1979d855818ca4643e4bea241d211de193
muteagony <mute.agony@gmail.com>
2022-01-02 20:36:22 +0100

    feat: add ingredient schema model

---
+++ supplier/models/ingredient_schema.py
@@ -0,0 +1,7 @@
+class IngredientSchema:
+    def __init__(self, id, name, season, cost_per_unit_of_quantity, preffered_shop):
+        self.id = id
+        self.name = name
+        self.season = season
+        self.cost_per_unit_of_quantity = cost_per_unit_of_quantity
+        self.preffered_shop = preffered_shop
